9a5f0922fe5ae66aefafef42455d041fb48148ea
David-123 <1826013250@qq.com>
2023-09-28 18:20:37 +0800

    添加自定义选项,更新提示

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # NBS2CMDBlock
 这个工具可以将你使用OpenNoteBlockStudio创作的工程文件导出为Minecraft中的结构NBT文件，并且可以使用结构方块读取
 
+# 使用方式
+使用如下命令安装第三方包
+```commandline
+python -m pip install pynbs amulet_nbt
+```
+将本项目克隆下来
+```commandline
+git clone https://github.com/1826013250/NBS2CMDBlock.git
+cd NBS2CMDBlock
+```
+之后运行主程序
+```commandline
+python main.py
+```
+
 # 为什么会有这个工具
 由于OpenNBS自身所携带的只有音符盒的结构导出方式，超音域的音符完全播放不了，而数据包的方式又有所偏离“红石音乐”的范畴。再者数据包本身的运作原理即为执行命令，那么完全可以使用命令方块代替，从而实现真正的红石音乐
 
@@ -18,5 +33,10 @@
 - 更加人性化的TUI
 - English Support
 
+# 欢迎提出issue！
 # 游戏内截屏
 ![screenshot0 - 在Minecraft内的截屏，展示了已经加载了两个结构的地图](./screenshot0.png)
+
+# 特别感谢
+[OpenNBS/PyNBS](https://github.com/OpenNBS/pynbs)
+[Amulet-NBT](https://github.com/Amulet-Team/Amulet-NBT)
--- main.py
+++ main.py
@@ -1,6 +1,39 @@
 import pynbs
-from amulet_nbt import CompoundTag as Compound, ListTag as List, IntTag as Int, StringTag as String
-from os.path import splitext
+from amulet_nbt import CompoundTag as Compound, ListTag as List, IntTag as Int, StringTag as String, ByteTag as Byte
+from os.path import splitext, splitdrive
+
+def wait_input(message, allow_empty=True, only_number=False, only_string=False, out_lowercase=False, strip="both",
+               number_range=False):
+    while True:
+        temp = input(message)
+        if not temp and allow_empty:
+            return temp
+        if only_number:
+            try:
+                int(temp)
+            except ValueError:
+                pass
+            else:
+                if number_range:
+                    if number_range[0] <= int(temp) <= number_range[-1]:
+                        return temp
+                else:
+                    return temp
+        elif only_string and temp.isalpha():
+            if out_lowercase:
+                temp = temp.lower()
+            if strip == "both":
+                temp = temp.strip()
+            elif strip == "left":
+                temp = temp.lstrip()
+            elif strip == "right":
+                temp = temp.rstrip()
+            else:
+                raise Exception("无效的类型 %s" % strip)
+            return temp
+        elif not only_number and not only_string:
+            return temp
+        print("输入不合规!%s" % ("请输入数字!" if only_number else "请仅输入字母!"))
 
 real_keys = {}
 for i in range(9):
@@ -41,13 +74,13 @@ structure = Compound({
     'blocks': List([]),
     'palette': List([
         Compound({
-            'Name': String("minecraft:air")
+            'Name': String("minecraft:air")  # id: 0
         }),
         Compound({
-            'Name': String("minecraft:stone")
+            'Name': String("minecraft:stone")  # id: 1
         }),
         Compound({
-            'Name': String("minecraft:repeater"),
+            'Name': String("minecraft:repeater"),  # id: 2
             'Properties': Compound({
                 'delay': String("1"),
                 'facing': String("west"),
@@ -56,7 +89,26 @@ structure = Compound({
             })
         }),
         Compound({
-            'Name': String("minecraft:command_block")
+            'Name': String("minecraft:command_block"),  # id: 3
+            'Properties': Compound({
+                'facing': String("east")
+            })
+        }),
+        Compound({
+            'Name': String("minecraft:oak_sign"),  # id: 4
+            'Properties': Compound({
+                'rotation': String("4")
+            })
+        }),
+        Compound({
+            'Name': String("minecraft:stone_button"),  # id: 5
+            'Properties': Compound({
+                'facing': String("west"),
+                'face': String("wall")
+            })
+        }),
+        Compound({
+            'Name': String("minecraft:redstone_lamp")  # id: 6
         })
     ])
 })
@@ -64,9 +116,20 @@ structure = Compound({
 filename = input("请输入文件路径: ")
 nbs = pynbs.read(filename)
 
+config_height = wait_input("请输入玩家悬浮高度(留空默认,默认为10): ", only_number=True)
+if not config_height:
+    config_height = 10
+
+config_offset = wait_input("请输入玩家相对于当前播放进度位置的偏移格数(留空默认,默认为-5): ", only_number=True)
+if not config_offset:
+    config_offset = -5
+
+config_play_location = wait_input("请选择播放时玩家位置:\n[1] 位于整个谱子正中间的上方\n[2] 位于最长轨道的上方\n> ",
+                                  only_number=True, number_range=[1, 2])
+
 print("载入NBS内自定义音色")
 for i in nbs.instruments:
-    instruments[i.id] = i.file
+    instruments[16 + i.id] = i.file
 
 print("载入NBS轨道...")
 meta = {}  # layer, note
@@ -98,29 +161,28 @@ for layer_id, value in cleaned.items():
                                             'volume': note['volume']}
 
 _progress_zone_setup = 6
-zone = []  # y x z
-for z in range(len(meta.keys()) * 2):
+_progress_zone_setup_max = len(cleaned.keys()) * 2 * 6 * (nbs.header.song_length + 1) * 2
+zone = []  # z y x， z//2代表轨道
+for z in range(len(cleaned.keys()) * 2):
     zone.append([[], [], [], [], [], []])
-    _progress_zone_setup += 6
-    for x in range(nbs.header.song_length * 2 + 1):
-        print("初始化方块矩阵...%d/%d" % (_progress_zone_setup,
-              len(meta.keys()) * 2 * 6 * (nbs.header.song_length + 1) * 2), end="\r")
+    for x in range((nbs.header.song_length + 1) * 2):
+        print("初始化方块矩阵...%d/%d" % (_progress_zone_setup, _progress_zone_setup_max), end="\r")
         if z % 2 == 0:
             if song.get(z // 2, False) and x // 2 < len(song[z // 2]):
                 zone[z][0].append({'state': 1})
                 _progress_zone_setup += 1
-                if x % 2 == 0:
+                if x % 2 == 1:
                     if song[z // 2][x // 2]:
                         zone[z][1].append({'state': 3, 'command':
                                            "execute as @a at @a run playsound "
                                            + instruments[song[z // 2][x // 2]['instrument']]
                                            + real_keys[song[z // 2][x // 2]['key']][0]
-                                           + " block @s ~ ~ ~ "
+                                           + " block @s ~1 ~ ~ "
                                            + str(song[z // 2][x // 2]['volume']) + " "
                                            + real_keys[song[z // 2][x // 2]['key']][1]})
                         _progress_zone_setup += 1
                     else:
-                        zone[z][1].append({'state': 1})
+                        zone[z][1].append({'state': 6})
                         _progress_zone_setup += 1
                 else:
                     zone[z][1].append({'state': 2})
@@ -139,22 +201,93 @@ for z in range(len(meta.keys()) * 2):
         zone[z][5].append({'state': 0})
         _progress_zone_setup += 4
 
+print("\n寻找最长轨道便于放置传送用命令方块")
+longest = None
+for i in range(len(zone)):
+    if zone[i][0][-1] != {'state': 0}:
+        longest = i
+if not longest:
+    print("出现错误！将不会创建传送用的命令方块轨道")
+else:
+    _progress_teleport_commandblock = 0
+    _progress_teleport_commandblock_max = len(zone[longest][0]) // 2
+    for i in range(len(zone[longest][0])):
+        if i % 2 == 1:
+            _progress_teleport_commandblock += 1
+            print("正在放置...%d/%d\r" % (_progress_teleport_commandblock, _progress_teleport_commandblock_max), end='')
+            zone[longest][0][i] = {
+                'state': 3,
+                'command': f"tp @a ~{config_offset} ~{config_height} ~%s" % (
+                    "" if config_play_location == '2' else f"{-longest + len(zone)//2 -1}"
+                )
+            }
+
+print("\n进行零碎方块放置")
+if len(zone) < 5:
+    while len(zone) < 5:
+        zone.append([[{'state': 0}, {'state': 0}, {'state': 0}, {'state': 0}, {'state': 0}, {'state': 0}],
+                     [{'state': 0}, {'state': 0}, {'state': 0}, {'state': 0}, {'state': 0}, {'state': 0}],
+                     [{'state': 0}, {'state': 0}, {'state': 0}, {'state': 0}, {'state': 0}, {'state': 0}],
+                     [{'state': 0}, {'state': 0}, {'state': 0}, {'state': 0}, {'state': 0}, {'state': 0}],
+                     [{'state': 0}, {'state': 0}, {'state': 0}, {'state': 0}, {'state': 0}, {'state': 0}]])
+for z in range(5):  # 布置小平台
+    for x in range(5):
+        zone[z][2][x] = {'state': 1}
+finalz = -2 + len(zone)
+zone[2][3][2] = {  # 放置启动用命令方块
+    'state': 3,
+    'command': "fill ~-3 ~-2 ~-2 ~-3 ~-2 ~%d minecraft:redstone_block" % finalz
+}
+zone[2][3][1] = {'state': 5}  # 放置启动按钮
+zone[1][1][0] = {  # 放置清除红石块的命令方块
+    'state': 3,
+    'command': "fill ~-1 ~ ~-1 ~-1 ~ ~%d minecraft:air" % (finalz + 1)
+}
+zone[2][4][2] = {
+    'state': 4,
+    'sign': True,
+    'glow': True,
+    'color': "black",
+    'message1': splitdrive(filename)[-1],
+    'message2': nbs.header.description,
+    'message3': f"红石刻速度: {nbs.header.tempo}tick/s",
+    'message4': f"推荐游戏刻{nbs.header.tempo * 2}"
+}
+
+
 print()
 _progress_zone_fill = 0
+_progress_zone_fill_max = len(zone) * len(zone[0]) * len(zone[0][0])
 for z in range(len(zone)):
     for y in range(len(zone[z])):
         for x in range(len(zone[z][y])):
             _progress_zone_fill += 1
-            print("填充方块...%d/%d" % (_progress_zone_fill, len(zone) * len(zone[0]) * len(zone[0][0])), end="\r")
+            print("填充方块...%d/%d" % (_progress_zone_fill, _progress_zone_fill_max), end="\r")
             block = Compound({
                 'pos': List([Int(x), Int(y), Int(z)]),
                 'state': Int(zone[z][y][x]['state']),
             })
-            if zone[z][y][x].get('command', ''):
-                block['nbt'] = Compound({'Command': String(zone[z][y][x].get('command', ''))})
+            if zone[z][y][x].get('command'):
+                block['nbt'] = Compound({'Command': String(zone[z][y][x]['command'])})
+            if zone[z][y][x].get('sign'):
+                block['nbt'] = Compound({
+                    'is_waxed': Byte(1),
+                    'front_text': Compound({
+                        'has_glowing_text': Byte(zone[z][y][x]['glow']),
+                        'color': String(zone[z][y][x]['color']),
+                        'messages': List([
+                            String(zone[z][y][x].get('message1', '{"text":""}')),
+                            String(zone[z][y][x].get('message2', '{"text":""}')),
+                            String(zone[z][y][x].get('message3', '{"text":""}')),
+                            String(zone[z][y][x].get('message4', '{"text":""}'))
+                        ])
+                    })
+                })
             structure['blocks'].append(block)
 print()
-with open(splitext(filename)[0].lower().replace(' ', '_')+".nbt", 'wb') as f:
+filename = splitext(filename)[0].lower().replace(' ', '_').replace("'", '_') + ".nbt"
+with open(filename, 'wb') as f:
     print("正在写入文件...")
     structure.save_to(f)
-print("完成！文件已保存在：\n%s" % splitext(filename)[0].lower().replace(' ', '_')+".nbt")
+print("完成！文件保存在：\n%s" % filename)
+print("本歌曲采用的红石刻速度为%d tick/s, 请在游戏内做适当调整" % nbs.header.tempo)
